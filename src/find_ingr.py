20fef8b4c3d22a57a81b8c082b1d4f726820a1aa
Checkmate50 <dgeisler50@gmail.com>
2018-07-12 22:53:41 -0400

    reformatting

--- src/find_ingr.py
+++ src/find_ingr.py
@@ -7,29 +7,31 @@ import sys
 import math
 import random
 
+
 def parse_table(table, terrain, rarity):
-	to_return = []
-	for item in table:
-		temp = item.strip().split("\t")
-		while len(temp)>3:
-			temp.pop(1)
-		if temp[2].lower().strip()==terrain:
-			if int(temp[1].lower().strip())==rarity:
-				to_return.append(temp[0])
-	return to_return
+    to_return = []
+    for item in table:
+        temp = item.strip().split("\t")
+        while len(temp) > 3:
+            temp.pop(1)
+        if temp[2].lower().strip() == terrain:
+            if int(temp[1].lower().strip()) == rarity:
+                to_return.append(temp[0])
+    return to_return
+
 
 terrain = sys.argv[1].lower().strip()
-rarity = 5-int(math.log(random.randint(1, 20), 2))
-amount = random.randint(15, 20)-3*rarity
+rarity = 5 - int(math.log(random.randint(1, 20), 2))
+amount = random.randint(15, 20) - 3 * rarity
 if amount <= 0:
-	amount = 1
+    amount = 1
 
 f = open("Ingredients.txt", 'r')
 ingr = parse_table(f, terrain, rarity)
-if len(ingr)==0:
-	name = "nothing"
-	amount = 0
+if len(ingr) == 0:
+    name = "nothing"
+    amount = 0
 else:
-	name = ingr[random.randint(0, len(ingr)-1)]
+    name = ingr[random.randint(0, len(ingr) - 1)]
 
 print(amount, name)
--- src/read_potion.py
+++ src/read_potion.py
@@ -7,28 +7,28 @@ import sys
 code = "61-17-24"
 p = code.split("-")
 if len(sys.argv) > 1:
-	p = sys.argv
-	p.pop(0)
-	for i in range(len(p)):
-		p[i] = int(p[i])
+    p = sys.argv
+    p.pop(0)
+    for i in range(len(p)):
+        p[i] = int(p[i])
 
 type_codes = []
 t = open("Types.txt", 'r')
 for line in t:
-	type_codes.append(line.split())
-	
+    type_codes.append(line.split())
+
 comp = []
 for i in p:
-	mod = (int(i)%4)+1
-	value = "I"*mod
-	if mod == 4:
-		value = "IV"
-	i = str(int(i)-mod+1)
-	name = ""
-	for j in type_codes:
-		if i == j[0]:
-			name = j[1]
-	comp.append(name + " " + value)
-	
+    mod = (int(i) % 4) + 1
+    value = "I" * mod
+    if mod == 4:
+        value = "IV"
+    i = str(int(i) - mod + 1)
+    name = ""
+    for j in type_codes:
+        if i == j[0]:
+            name = j[1]
+    comp.append(name + " " + value)
+
 for i in comp:
-	print(i)
+    print(i)
